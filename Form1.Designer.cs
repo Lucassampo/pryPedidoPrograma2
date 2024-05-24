87986be330ee38e4d85876227833dedeab90e7c4
Alumno <Alumno@PC7>
2024-05-24 13:24:50 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace pryPedidoPrograma2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblIniciosSesion = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblContraseña = new System.Windows.Forms.Label();
+            this.txtUsuarios = new System.Windows.Forms.TextBox();
+            this.txtContraseñas = new System.Windows.Forms.TextBox();
+            this.btnInciar = new System.Windows.Forms.Button();
+            this.btnCrearUsuario = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblIniciosSesion
+            // 
+            this.lblIniciosSesion.AutoSize = true;
+            this.lblIniciosSesion.Location = new System.Drawing.Point(7, 9);
+            this.lblIniciosSesion.Name = "lblIniciosSesion";
+            this.lblIniciosSesion.Size = new System.Drawing.Size(67, 13);
+            this.lblIniciosSesion.TabIndex = 0;
+            this.lblIniciosSesion.Text = "Inicio Sesion";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(16, 49);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(43, 13);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblContraseña
+            // 
+            this.lblContraseña.AutoSize = true;
+            this.lblContraseña.Location = new System.Drawing.Point(13, 80);
+            this.lblContraseña.Name = "lblContraseña";
+            this.lblContraseña.Size = new System.Drawing.Size(61, 13);
+            this.lblContraseña.TabIndex = 2;
+            this.lblContraseña.Text = "Contraseña";
+            // 
+            // txtUsuarios
+            // 
+            this.txtUsuarios.Location = new System.Drawing.Point(65, 46);
+            this.txtUsuarios.Name = "txtUsuarios";
+            this.txtUsuarios.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuarios.TabIndex = 3;
+            // 
+            // txtContraseñas
+            // 
+            this.txtContraseñas.Location = new System.Drawing.Point(80, 77);
+            this.txtContraseñas.Name = "txtContraseñas";
+            this.txtContraseñas.Size = new System.Drawing.Size(100, 20);
+            this.txtContraseñas.TabIndex = 4;
+            // 
+            // btnInciar
+            // 
+            this.btnInciar.Location = new System.Drawing.Point(16, 124);
+            this.btnInciar.Name = "btnInciar";
+            this.btnInciar.Size = new System.Drawing.Size(75, 36);
+            this.btnInciar.TabIndex = 5;
+            this.btnInciar.Text = "Iniciar";
+            this.btnInciar.UseVisualStyleBackColor = true;
+            this.btnInciar.Click += new System.EventHandler(this.btnInciar_Click);
+            // 
+            // btnCrearUsuario
+            // 
+            this.btnCrearUsuario.Location = new System.Drawing.Point(119, 124);
+            this.btnCrearUsuario.Name = "btnCrearUsuario";
+            this.btnCrearUsuario.Size = new System.Drawing.Size(75, 36);
+            this.btnCrearUsuario.TabIndex = 6;
+            this.btnCrearUsuario.Text = "Crear Usuario";
+            this.btnCrearUsuario.UseVisualStyleBackColor = true;
+            this.btnCrearUsuario.Click += new System.EventHandler(this.btnCrearUsuario_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 172);
+            this.Controls.Add(this.btnCrearUsuario);
+            this.Controls.Add(this.btnInciar);
+            this.Controls.Add(this.txtContraseñas);
+            this.Controls.Add(this.txtUsuarios);
+            this.Controls.Add(this.lblContraseña);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.lblIniciosSesion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblIniciosSesion;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblContraseña;
+        private System.Windows.Forms.TextBox txtUsuarios;
+        private System.Windows.Forms.TextBox txtContraseñas;
+        private System.Windows.Forms.Button btnInciar;
+        private System.Windows.Forms.Button btnCrearUsuario;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using pryInicioSesionLogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryPedidoPrograma2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        clsUsuario Usuarios = new clsUsuario();
+        string Usuario;
+        string Contraseña;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCrearUsuario_Click(object sender, EventArgs e)
+        {
+             Usuario = txtUsuarios.Text;
+             Contraseña = txtContraseñas.Text;
+             
+
+            Usuarios.RegistroLogInicioSesion();
+
+        }
+
+        private void btnInciar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
